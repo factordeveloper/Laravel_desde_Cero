9fedf46b7c3919871b5bb1e68c0bea3adaaa782c
Felipe Herrera Pinzon <felipeherrerapinzon@gmail.com>
2023-08-10 16:12:39 -0500

    video 28, pasar datos a vistas

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -26,13 +26,13 @@ class ProductController extends Controller
 
     public function show($product){
 
-    // $product = Product::find($product);   // null si no existe id
-
      $product = Product::findOrFail($product);  // 404 NOT FOUND si no existe id
-     //return $product;
-     dd($product);
 
-    return view('products.show');
+     //dd($product);
+
+    return view('products.show')->with([
+        'element' => $product,
+    ]);
         
     }
 
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -6,7 +6,10 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Jabon</h1>
-    <h3>el mejor jabon de todos</h3>
+    <h1>{{ $element-> title }} ({{ $element-> id }})</h1>
+    <h3>{{ $element-> description }}</h3>
+    <h3>{{ $element-> price }}</h3>
+    <h3>{{ $element-> stock }}</h3>
+    <h3>{{ $element-> status }}</h3>
 </body>
 </html>
